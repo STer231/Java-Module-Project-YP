8a0cbb90af655a28ea8f2f4bb83abedafa9ff9d0
STer <igorrozembaum@yandex.ru>
2024-08-31 17:10:02 +0300

    1. Реализован ввод данных от пользователя 2. Сформирован метод, определяющий корректность введеных данных 3. Реализован метод, определяющий победителя гонки 4. Реализован вывод победителя гонки

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,25 @@
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Race race = new Race();
+        for (int i = 1; i <= 3; i++) {
+            String carName = "";
+            int carSpeed = 0;
+
+            Input inputName = new Input();
+            Input inputSpeed = new Input();
+            carName = inputName.getStringValue(
+                    "Введите название машины № " + i + " ",
+                    "Введено некорректное значение");
+
+            carSpeed = inputSpeed.getIntegerValue(
+                    "Введите скорость машины № " + i + " ",
+                    "Введено некорректное значение",
+                    0,
+                    250);
+            Car car = new Car(carName, carSpeed);
+            race.checkLeader(car);
+        }
+        System.out.println("Победитель гонки " + race.getLeader().name);
     }
 }
